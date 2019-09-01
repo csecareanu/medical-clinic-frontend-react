8ec16635afa0e940e17668a00e5fbd0ad05779cd
cristi <cristi.secareanu@yahoo.com>
2019-09-01 15:21:16 +0300

    Added react-intl

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import {FormattedMessage} from 'react-intl';
+
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+    <h1>
+      <FormattedMessage  id="App.titleMessage"  defaultMessage="Medical center website."/>
+    </h1>
+ );
 }
 
 export default App;
