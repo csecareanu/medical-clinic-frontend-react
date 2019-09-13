6f9c53b09465fe443e10b452ef48d91c0bbc65a4
cristi <cristi.secareanu@yahoo.com>
2019-09-13 22:38:19 +0300

    added internationalization form the main page

--- src/components/menus/PatientMainMenuView/PatientMainMenuView.js
+++ src/components/menus/PatientMainMenuView/PatientMainMenuView.js
@@ -1,19 +1,40 @@
 
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
 
 import NavListView from '../../UI/NavListView/NavListView';
-import NavItemView from '../../UI/NavListView/NavItemButton/NavItemButton';
+import NavItemButton from '../../UI/NavListView/NavItemButton/NavItemButton';
+import classesDesktop from '../desktop.module.css';
 
+//TODO: provide style={classesDesktop} to NavItemButton in a better way
 export default () => (
-    <NavListView>
-        <NavItemView link="/">HOME</NavItemView>
-        <NavItemView link="/doctors">DOCTORS</NavItemView>
-        <NavItemView link="/about">ABOUT CLINIC</NavItemView>
-        <NavItemView link="/contact">CONTACT</NavItemView>
-        <NavItemView link="/prices">PRICES</NavItemView>
-        <NavItemView link="/appointment">NEW APPOINTMENT</NavItemView>
-        <NavItemView link="/my_account">MY ACCOUNT</NavItemView>
-        <NavItemView link="/login">LOGIN</NavItemView>
-        <NavItemView link="/logout">LOGOUT</NavItemView>
+    <NavListView style={classesDesktop}>
+        <NavItemButton link="/" style={classesDesktop} >
+            <FormattedMessage id="menu_home" defaultMessage={'HOME'}/>
+        </NavItemButton>
+        <NavItemButton link="/doctors" style={classesDesktop} >
+            <FormattedMessage id="menu_doctors" defaultMessage={'DOCTORS'}/>
+        </NavItemButton>
+        <NavItemButton link="/about" style={classesDesktop}>
+            <FormattedMessage id="menu_about_clinic" defaultMessage={'ABOUT CLINIC'}/>
+        </NavItemButton>
+        <NavItemButton link="/contact" style={classesDesktop}>
+            <FormattedMessage id="menu_contact" defaultMessage={'CONTACT'}/>
+        </NavItemButton>
+        <NavItemButton link="/prices"style={classesDesktop} >
+            <FormattedMessage id="menu_prices" defaultMessage={'PRICES'}/>
+        </NavItemButton>
+        <NavItemButton link="/appointment" style={classesDesktop}>
+            <FormattedMessage id="menu_new_appointment" defaultMessage={'NEW APPOINTMENT'}/>
+        </NavItemButton>
+        <NavItemButton link="/my_account" style={classesDesktop}>
+            <FormattedMessage id="menu_my_account" defaultMessage={'MY ACCOUNT'}/>
+        </NavItemButton>
+        <NavItemButton link="/login" style={classesDesktop}>
+            <FormattedMessage id="menu_login" defaultMessage={'LOGIN'}/>
+        </NavItemButton>
+        <NavItemButton link="/logout" style={classesDesktop}>
+            <FormattedMessage id="menu_logout" defaultMessage={'LOGOUT'}/>
+        </NavItemButton>
     </NavListView>
 );
