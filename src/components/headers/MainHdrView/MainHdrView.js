d14890d2b7e840928d4cfc15cac7ab63a09e45f7
cristi <cristi.secareanu@yahoo.com>
2019-09-17 16:52:07 +0300

    1. name reactory for AccountToolbarView  toolbar. 2. Chaned menu color for admin rights

--- src/components/headers/MainHdrView/MainHdrView.js
+++ src/components/headers/MainHdrView/MainHdrView.js
@@ -4,9 +4,9 @@ import classes from './MainHdrView.module.css';
 import imgMain from '../../../static/images/main_hdr.jpg';
 import MainImageView from '../common/MainImageView/MainImageView';
 import ClinicMenuView from '../../menus/ClinicMenuView/ClinicMenuView';
-//import DoctorMenuView from '../../menus/DoctorMenuView/DoctorMenuView';
+import DoctorMenuView from '../../menus/DoctorMenuView/DoctorMenuView';
 import AboutClinicToolbarView from '../../toolbars/AboutClinicToolbarView/AboutClinicToolbarView';
-import AccountToolbarView from '../../toolbars/AccountToolbarView/AccountToolbarView';
+import UserAccountToolbarView from '../../toolbars/UserAccountToolbarView/UserAccountToolbarView';
 import MenuToolbarView from '../../toolbars/MenuToolbarView/MenuToolbarView';
 
 
@@ -15,9 +15,9 @@ export default () => (
         <nav>
             <AboutClinicToolbarView />
             <div className={classes.ToolbarDistance} />
-            <AccountToolbarView />
+            <UserAccountToolbarView />
             <MenuToolbarView>
-               {/* <DoctorMenuView /> */}
+                <DoctorMenuView />
                 <ClinicMenuView />
             </MenuToolbarView>
             <MainImageView src={imgMain} />
--- src/components/toolbars/UserAccountToolbarView/UserAccountToolbarView.js
+++ src/components/toolbars/UserAccountToolbarView/UserAccountToolbarView.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import classes from './AccountToolbarView.module.css';
+import classes from './UserAccountToolbarView.module.css';
 import NewAppointmentToolbarItemView from './NewAppointmentToolbarItemView/NewAppointmentToolbarItemView';
 import MyAccountToolbarItemView from './MyAccountToolbarItemView/MyAccountToolbarItemView';
 
