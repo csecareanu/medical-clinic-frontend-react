fdab6f1959b210a16508ddff6cbc38c5f4e75bf7
cristi <cristi.secareanu@yahoo.com>
2019-09-17 20:02:46 +0300

    Updated DoctorAdminHdrView

--- src/components/headers/DoctorAdminHdrView/DoctorAdminHdrView.js
+++ src/components/headers/DoctorAdminHdrView/DoctorAdminHdrView.js
@@ -1,12 +1,29 @@
-
 import React from 'react';
+
+import classes from './DoctorAdminHdrView.module.css';
+import commonClasses from '../../../common.module.css';
 import DoctorAdminMenuView from '../../menus/DoctorAdminMenuView/DoctorAdminMenuView';
+import AboutClinicToolbarView from '../../toolbars/AboutClinicToolbarView/AboutClinicToolbarView';
+import UserToolbarView from '../../toolbars/UserToolbarView/UserToolbarView';
+import MenuToolbarView from '../../toolbars/MenuToolbarView/MenuToolbarView';
+import MenuToggleToolbarView from '../../toolbars/MenuToggleToolbarView/MenuToggleToolbarView';
 
 export default () => (
-    <header>
-        <h1>Doctor Admin Header</h1>
+    <header className={classes.Header}>
         <nav>
-            <DoctorAdminMenuView />
+            <AboutClinicToolbarView />
+            <div className={classes.ToolbarVSeparator} />
+            <div className={classes.MenuToggle_UserAccount_Toolbars}>
+                <div className={commonClasses.SmallScreenOnly}>
+                    <MenuToggleToolbarView />
+                </div>
+                <UserToolbarView />
+            </div>
+            <div className={commonClasses.LargeScreenOnly} >
+                <MenuToolbarView>
+                    <DoctorAdminMenuView />
+                </MenuToolbarView>
+            </div>
         </nav>
     </header>
 );
