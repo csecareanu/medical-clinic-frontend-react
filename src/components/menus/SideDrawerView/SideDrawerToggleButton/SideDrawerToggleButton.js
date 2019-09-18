0b66901c1e50f2113447526e29620320ab9a26ae
cristi <cristi.secareanu@yahoo.com>
2019-09-18 18:29:38 +0300

    1. Added 'white-space: nowrap;' for menus. 2. Small visbility improvement for SideDrawer button

--- src/components/menus/SideDrawerView/SideDrawerToggleButton/SideDrawerToggleButton.js
+++ src/components/menus/SideDrawerView/SideDrawerToggleButton/SideDrawerToggleButton.js
@@ -3,11 +3,9 @@ import React from 'react';
 import classes from './SideDrawerToggleButton.module.css';
 
 export default (props) => (
-    <div className={classes.MenuToggle}>
-        <div className={classes.ToggleSign} onClick={props.clickHandler}>
-            <div />
-            <div />
-            <div />
-        </div>
+    <div className={classes.Button} onClick={props.clickHandler}>
+        <div />
+        <div />
+        <div />
     </div>
 );
